0b210464d14f651fdb7efbd630e1abdf318a4ae5
Logan <58321687+Smyli3@users.noreply.github.com>
2022-08-07 20:02:02 -0700

    Day 1

--- encode.py
+++ encode.py
@@ -1,2 +1,24 @@
 def main():
-    target = input("What is the string you want to encode?:\n")
+    target = input("What is the string you want to encode?:\n").lower()
+    sauce = {
+        "a": " .-",
+        "b": " -...",
+        "c":"",
+        "d":"",
+        "e":"",
+        "f":"",
+        "g":"",
+        "h":"",
+        "i":"",
+        "j":"",
+        "k":"",
+        "l":"",
+        "m":"",
+        
+        
+    }
+    new =''
+    for i in target:
+        new = new + sauce[i]
+    print(new)
+        
